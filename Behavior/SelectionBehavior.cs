124a27d395a404a55481692b5d2000df722889dc
MishaMexxx <52034704+MishaMexxx@users.noreply.github.com>
2019-06-20 16:21:08 +0300

    Add files via upload
    Behavior

---
+++ Behavior/SelectionBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+using BaseFrame;
+
+namespace DBClient
+{
+	public class SelectionBehavior : Behavior<DataGrid>
+	{
+		public static readonly DependencyProperty CommandProperty =
+			DependencyProperty.Register("Command", typeof(ICommand), typeof(SelectionBehavior), new PropertyMetadata(default(string)));
+
+		public static readonly DependencyProperty CurrentProperty =
+		DependencyProperty.Register("CurrentData", typeof(ViewModelBase), typeof(SelectionBehavior), new PropertyMetadata(default(string)));
+
+		public ICommand Command
+		{
+			get => (ICommand) GetValue(CommandProperty);
+			set => SetValue(CommandProperty, value);
+		}
+
+		public ViewModelBase CurrentData
+		{
+			get => (ViewModelBase) GetValue(CurrentProperty);
+			set => SetValue(CommandProperty, value);
+		}
+
+		protected override void OnAttached()
+		{
+			AssociatedObject.SelectionChanged += this.AssociatedObject_SelectionChanged;
+		}
+
+		private void AssociatedObject_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			Command?.Execute(CurrentData);
+		}
+
+		protected override void OnDetaching()
+		{
+			AssociatedObject.SelectionChanged -= AssociatedObject_SelectionChanged;
+		}
+
+	}
+}
---
+++ Behavior/StadiumsDataGridBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace DBClient
+{
+	public class StadiumsDataGridBehavior : Behavior<DataGrid>
+	{
+		public static readonly DependencyProperty CommandProperty =
+			DependencyProperty.Register("Command", typeof(ICommand), typeof(StadiumsDataGridBehavior), new PropertyMetadata(default(string)));
+
+		public static readonly DependencyProperty MatchProperty =
+		DependencyProperty.Register("Stadium", typeof(StadiumInfo), typeof(StadiumsDataGridBehavior), new PropertyMetadata(default(string)));
+
+		public ICommand Command
+		{
+			get => (ICommand) GetValue(CommandProperty);
+			set => SetValue(CommandProperty, value);
+		}
+
+		public StadiumInfo Stadium
+		{
+			get => (StadiumInfo) GetValue(MatchProperty);
+			set => SetValue(CommandProperty, value);
+		}
+
+		protected override void OnAttached()
+		{
+			AssociatedObject.SelectionChanged += this.AssociatedObject_SelectionChanged;
+		}
+
+		private void AssociatedObject_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			Command?.Execute(Stadium);
+		}
+
+		protected override void OnDetaching()
+		{
+			AssociatedObject.SelectionChanged -= AssociatedObject_SelectionChanged;
+		}
+
+	}
+}
